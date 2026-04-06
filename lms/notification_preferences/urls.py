8118646e0c9639f6a2d4488cff61ed51a81bf271
Tokurah Ibrahim <tokurah10@gmail.com>
2026-04-06 12:36:55 +0100

    refactored urls.py for my notification_preferences app

--- lms/notification_preferences/urls.py
+++ lms/notification_preferences/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import NotificationPreferenceView
 
 urlpatterns = [
-    path('notifications/preferences/', NotificationPreferenceView.as_view(), name='notification-preferences'),
+    path('settings/notifications/preferences/', NotificationPreferenceView.as_view(), name='notification-preferences'),
 ]
